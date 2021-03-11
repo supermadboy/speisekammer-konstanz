4d2ed4312f774cb5a189bb9a11d3f870854917f5
markus <markusszar@gmail.com>
2021-03-12 00:16:57 +0100

    finalize footer, add favicon, improve svgs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles/styles.scss';
-import $ from 'jquery'
 
 import './assets/images/png/succ.png';
 
@@ -35,4 +34,82 @@ document.addEventListener('DOMContentLoaded', () => {
 
   var currentSeason = getCurrentSeason();
   logo.classList.add(currentSeason);
+
+  var newsletterSignupButton = document.getElementById('newsletter_signup');
+  newsletterSignupButton.onclick = function() {
+    var emailToSignUp = document.getElementById('newsletter_email').value;
+
+    console.log(emailToSignUp);
+    var mapForm = document.createElement('form');  
+    mapForm.target = '_blank';  
+    mapForm.method = 'POST';
+    mapForm.action = 'https://ml.kundenserver.de/cgi-bin/mailinglist.cgi';
+    mapForm.style.display = 'none';
+
+    var radiosubscribe = document.createElement('input');
+    radiosubscribe.type = 'radio';
+    radiosubscribe.name = 'subscribe_r';
+    radiosubscribe.value = 'subscribe';
+    radiosubscribe.checked = true;
+
+    var radiounsubscribe = document.createElement('input');
+    radiounsubscribe.type = 'radio';
+    radiounsubscribe.name = 'subscribe_r';
+    radiounsubscribe.value = 'unsubscribe';
+
+    var mailaccount_r = document.createElement('input');
+    mailaccount_r.type = 'text';
+    mailaccount_r.name = 'mailaccount_r';
+    mailaccount_r.value = emailToSignUp;
+
+    var mailaccount2_r = document.createElement('input');
+    mailaccount2_r.type = 'text';
+    mailaccount2_r.name = 'mailaccount2_r';
+    mailaccount2_r.value = emailToSignUp;
+
+    var FBMLNAME = document.createElement('input');
+    FBMLNAME.type = 'text';
+    FBMLNAME.name = 'FBMLNAME';
+    FBMLNAME.value = 'newsletter@speisekammer-konstanz.de';
+
+    var FBLANG = document.createElement('input');
+    FBLANG.type = 'hidden';
+    FBLANG.name = 'FBLANG';
+    FBLANG.value = 'de';
+
+    var FBURLERROR = document.createElement('input');
+    FBURLERROR.type = 'hidden';
+    FBURLERROR.name = 'FBURLERROR_L';
+    FBURLERROR.value = 'https://ml.kundenserver.de/mailinglist/error.de.html';
+
+    var FBURLSUBSCRIBE = document.createElement('input');
+    FBURLSUBSCRIBE.type = 'hidden';
+    FBURLSUBSCRIBE.name = 'FBURLSUBSCRIBE_L';
+    FBURLSUBSCRIBE.value = 'https://ml.kundenserver.de/mailinglist/subscribe.de.html';
+
+    var FBURLUNSUBSCRIBE = document.createElement('input');
+    FBURLUNSUBSCRIBE.type = 'hidden';
+    FBURLUNSUBSCRIBE.name = 'FBURLUNSUBSCRIBE_L';
+    FBURLUNSUBSCRIBE.value = 'https://ml.kundenserver.de/mailinglist/unsubscribe.de.html';
+
+    var FBURLINVALID = document.createElement('input');
+    FBURLINVALID.type = 'hidden';
+    FBURLINVALID.name = 'FBURLINVALID_L';
+    FBURLINVALID.value = 'https://ml.kundenserver.de/mailinglist/invalid.de.html';
+
+    mapForm.appendChild(radiosubscribe);
+    mapForm.appendChild(radiounsubscribe);
+    mapForm.appendChild(mailaccount_r);
+    mapForm.appendChild(mailaccount2_r);
+    mapForm.appendChild(FBMLNAME);
+    mapForm.appendChild(FBLANG);
+    mapForm.appendChild(FBURLERROR);
+    mapForm.appendChild(FBURLSUBSCRIBE);
+    mapForm.appendChild(FBURLUNSUBSCRIBE);
+    mapForm.appendChild(FBURLINVALID);
+
+    document.body.appendChild(mapForm);
+    mapForm.submit();
+  }
 });
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -28,6 +29,11 @@ module.exports = {
       template: 'src/views/layouts/index.hbs',
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/assets/images/favicons", to: "./" },
+      ],
+    }),
   ],
   optimization: {
     minimize: true,
